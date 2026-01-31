1bdfb4dfc8d8cbbbdcfbd15b7dbe672ffebc1d25
Ratneshkv1711 <ratnesh.techy77@gmail.com>
2026-01-31 23:55:43 +0530

    Refactor AuthResponse class with Lombok annotations

--- shop-backend/src/main/java/com/example/shop/dto/AuthResponse.java
+++ shop-backend/src/main/java/com/example/shop/dto/AuthResponse.java
@@ -6,8 +6,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+
+    private String accessToken;
+    private String tokenType = "Bearer";
+}
 
 
-/**
- * RATNESH
- */
+
